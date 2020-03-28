834f44965ff23422e20c39322bd7914c3b4b9842
Simon <simon.chardon@yahoo.fr>
2020-03-28 17:22:31 +0100

    adding style to the home page

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import logo_home from '../assets/images/logo_home.svg'
+import Background from '../assets/images/background.png'
 
 class Home extends Component {
     render() {
         return (
-            <div className='home'>
-                <div className="logo">
+            <div className='home h-screen' >
+                <div className=" flex items-center flex-col">
                     <img src={logo_home} alt="logo HeartShare" height='400' width='400'/>
-                    <div className='connect'>
-                        <h3>connexion</h3>
+                    <div className='mt-20 p-5 bg-red-600 w-1/2 max-w-screen-md rounded-lg '>
+                        <p className="text-xl font-bold  text-center">connexion</p>
+                        <div className=" w-3/4 max-w-lg mx-auto">
+                            <form className=" px-8 pt-6 pb-8 mb-4">
+                                <div className="mb-4">
+                                <label className="block  text-sm font-bold mb-2" for="username">
+                                    Username
+                                </label>
+                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+                                </div>
+                                <div className="mb-6">
+                                <label className="block  text-sm font-bold mb-2" for="password">
+                                    Password
+                                </label>
+                                <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-900 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                                </div>
+                                <div className="flex items-center justify-between">
+                                <button className="bg-white hover:bg-red-200 text-red-600 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                                    Sign In
+                                </button>
+                                <a className="inline-block align-baseline font-bold text-sm text-white hover:text-red-200" href="#">
+                                    Forgot Password?
+                                </a>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import './assets/stylesheets/home.scss';
+import './assets/stylesheets/application.scss';
 
 //import component
 import Home from './components/home'
