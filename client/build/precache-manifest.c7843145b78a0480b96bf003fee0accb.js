4fcd8079710df80ff47a79702fafdc7fc0fc1453
Simon <simon.chardon@yahoo.fr>
2020-04-19 20:47:41 +0200

    card show done

---
+++ client/build/precache-manifest.c7843145b78a0480b96bf003fee0accb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ba31fc9ab49fceca15b9c9ee736201",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86da4f86d8d7222951a2",
+    "url": "/static/css/main.f1a930f8.chunk.css"
+  },
+  {
+    "revision": "7956c4ddea1c20ebec59",
+    "url": "/static/js/2.54516e23.chunk.js"
+  },
+  {
+    "revision": "07a7d105c1f375b2fe95256acae1b056",
+    "url": "/static/js/2.54516e23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86da4f86d8d7222951a2",
+    "url": "/static/js/main.ee682e4c.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "1637ffc2eafc89f91c2366b0988c54b7",
+    "url": "/static/media/actu.1637ffc2.jpg"
+  },
+  {
+    "revision": "ef9dc2c5e54c0e983e83b22e781746b0",
+    "url": "/static/media/background.ef9dc2c5.png"
+  },
+  {
+    "revision": "93aeafb5363cdb01fad2587caa79c408",
+    "url": "/static/media/logo-heart.93aeafb5.svg"
+  },
+  {
+    "revision": "ffb43085af40ca4487d5e538bb8f4488",
+    "url": "/static/media/logo_home.ffb43085.svg"
+  }
+]);
